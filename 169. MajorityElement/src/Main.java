0ecea54224d5b6dfd3068082b0fba2281cbfcd69
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-02-28 19:33:18 +0700

    add solution 2 for 169

--- 169. MajorityElement/src/Main.java
+++ 169. MajorityElement/src/Main.java
@@ -1,12 +1,33 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
+    //solution 1
     public static int majorityElement(int[] nums) {
         Arrays.sort(nums);
-        return nums[nums.length/2];
+        return nums[nums.length / 2];
     }
+
+    //solution 2
+    public static int majorityElement2(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for (int i : map.keySet()) {
+            if (map.get(i) > n / 2) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {2,2,1,1,1,1,2,2};
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
         System.out.println(majorityElement(nums));
+        majorityElement2(nums);
+        System.out.println(majorityElement2(nums));
     }
 }
