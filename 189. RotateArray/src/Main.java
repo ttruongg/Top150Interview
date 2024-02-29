7f77d2b10fca7037894d3de002498381ecc2ddd3
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-02-29 22:21:33 +0700

    add solution for 189

--- 189. RotateArray/src/Main.java
+++ 189. RotateArray/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
 public class Main {
+
+
+    public static void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+
+        }
+    }
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        reverse(nums, 0, n - 1);
+        reverse(nums,0,k-1);
+        reverse(nums,k, n - 1);
+
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {1,2,3,4,5,6,7};
+        rotate(nums, 3);
+        System.out.println(Arrays.toString(nums));
+
     }
 }
