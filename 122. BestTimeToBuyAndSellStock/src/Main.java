b1ab8f23f35474f0912055e954250cdf3aaa20bb
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-03-07 14:39:24 +0700

    add solution 122

--- 122. BestTimeToBuyAndSellStock/src/Main.java
+++ 122. BestTimeToBuyAndSellStock/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+    public  static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int n = prices.length;
+        boolean isBuy = false;
+        for (int i= 1 ; i < n; i++){
+            if (prices[i] > prices[i-1])
+            maxProfit += prices[i] - prices[i-1];
+        }
+        return maxProfit;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] prices = {1,2,3,4,5};
+        System.out.println(maxProfit(prices));
     }
 }
