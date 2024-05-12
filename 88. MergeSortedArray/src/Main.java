d4fd131d55a29841902fe11cdd29442a84d0a74a
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-05-12 21:57:40 +0700

    format code for solution 88

--- 88. MergeSortedArray/src/Main.java
+++ 88. MergeSortedArray/src/Main.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,19 +5,20 @@ import java.util.Arrays;
 public class Main {
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-        while (j >= 0 ){
-            if (i >= 0 && nums1[i] > nums2[j]){
-                nums1[k] = nums1[i];
-                k--;
-                i--;
+
+        int a = m - 1;
+        int b = n - 1;
+        int c = m + n - 1;
+        while ( b >= 0) {
+            if (a >= 0 && nums1[a] > nums2[b]){
+                nums1[c] = nums1[a];
+                a--;
             } else {
-                nums1[k] = nums2[j];
-                k--;
-                j--;
+                nums1[c] = nums2[b];
+                b--;
             }
+            c--;
+
         }
 
         System.out.println(Arrays.toString(nums1));
@@ -28,8 +28,8 @@ public class Main {
 
     public static void main(String[] args) {
         int m = 3, n = 3;
-        int[] arr1 = {1,2,3,0,0,0};
-        int[] arr2 = {2,5,6};
+        int[] arr1 = {1, 2, 3, 0, 0, 0};
+        int[] arr2 = {2, 5, 6};
         merge(arr1, m, arr2, n);
 
 
