1cd171cbc5e93cd9d521002d1fce16033d40a597
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-06-18 23:12:47 +0700

    solution for 55

--- 55. JumpGame/src/Main.java
+++ 55. JumpGame/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
+
+    public static boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+        for (int i = nums.length - 1; i > -1; i--) {
+            if (i + nums[i] >= goal) goal = i;
+
+        }
+
+        if (goal == 0) return true;
+        return false;
+
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] a = {3, 2, 1, 0, 4};
+        System.out.println(canJump(a));
     }
 }
