01fd07418dd38e62b5808711982c3dd2c4ecabde
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-12-19 00:57:28 +0700

    soluction for 20.valid parentheses

---
+++ 20. Valid Parentheses/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class Main {
+    public static boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+}
